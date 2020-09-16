c5458662cc85ad35587746f472cbe145585b6240
kamarenmccord <kamarenmccord@yahoo.com>
2020-09-15 21:50:34 -0500

    inventory foundations add and remove

--- logics/inventory.py
+++ logics/inventory.py
@@ -1,8 +1,15 @@
 
+from .items import *
+
 class Inventory:
     # default class for the players inventory
     def __init__(self):
-        self.all_pockets = {'misc': [], 'key_items': [], 'healing': [], 'weapons': []}
+        self.all_pockets = {
+                            'misc': [],
+                            'key_items': [],
+                            'healing': [],
+                            'weapons': []
+                            }
 
     def __str__(self):
         a_list = []
@@ -13,7 +20,16 @@ class Inventory:
 
     def add(self, something):
         # add something to a given pocket
-        for key, value in something.items():
-            for pocket, contents in self.all_pockets.items():
-                if key == pocket:
-                    pocket.contents.append(value)
+        for key in self.all_pockets.keys():
+            if key == something.pouch:
+                self.all_pockets[f'{key}'].append(something)
+
+    def remove(self, something):
+        # remove an item obj from a pouch
+        for key, value in self.all_pockets.items():
+            index = 0
+            for listItem in value:
+                if something.name == listItem.name:
+                    del self.all_pockets[f'{key}'][index]
+                    break  # prevents multiple of like items from being removed
+                index += 1
---
+++ logics/items.py
@@ -0,0 +1,41 @@
+
+import pygame
+
+""" holds item classes for the game """
+
+
+class Item:
+    def __init__(self, pouch, name, description, value):
+        self.pouch = pouch  # key_items, weapons, healing, misc
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def __str__(self):
+        return self.name
+
+
+class Potion(Item):
+    def __init__(self):
+        pouch = 'healing'
+        name = 'potion'
+        value = 5
+        description = """ a questionable drink that builds constitution """
+        super().__init__(pouch, name, description, value)
+
+
+class FireBallScroll(Item):
+    def __init__(self):
+        pouch = 'misc'
+        name = "Scroll of FireBall"
+        value = 30
+        desc = """ an old parchment with a glowing red symbol in the centre """
+        super().__init__(pouch, name, desc, value)
+
+
+class Elixr(Item):
+    def __init__(self):
+        super().__init__(pouch='misc',
+                         name='Elixr',
+                         description='an old parchment with a yellow glow in the centre',
+                         value=30)
