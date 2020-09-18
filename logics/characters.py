9760f3406f5d149b4a6252f449fd1aee4aaf048e
kamarenmccord <kamarenmccord@yahoo.com>
2020-09-18 09:41:19 -0500

    scaled npc

--- logics/characters.py
+++ logics/characters.py
@@ -226,6 +226,7 @@ class Npc(pygame.sprite.Sprite):
         self.pos = x+16, y+16
         self.game = game
         self.image = pygame.image.load(path.join(game.game_folder, img))
+        self.image = pygame.transform.scale(self.image, (48, 81))
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
         self.interact_points = self.make_interact_points(interact=interact)
